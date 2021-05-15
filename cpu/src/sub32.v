f2eb945fb1e3ae9acc4374af4bf2ef03d9a8c95c
Gabor <gaborpelesz@gmail.com>
2021-05-15 21:39:47 +0200

    refactor sub32 module

--- cpu/src/sub32.v
+++ cpu/src/sub32.v
@@ -1,21 +1,22 @@
 `timescale 1ns / 1ps
 
-module mSUB32(
-    input  wire [31:0] IN1, IN2,
-           wire CIN, // carry input, set it to HIGH to have no affect (in SUB)
-    output wire [31:0] OUT,
-           wire COUT, // carry output
-           wire OVERFLOW
+module sub32(
+    input  wire [31:0] in_data0, 
+           wire [31:0] in_data1,
+           wire        in_carry, // carry input, set it to HIGH to have no affect (in SUB)
+    output wire [31:0] out_data,
+           wire        out_carry, // carry output
+           wire        out_overflow
     );
     
-    wire [32:0] tmp_OUT;
+    wire [32:0] result;
     
-    assign tmp_OUT = IN1 - IN2 - ~CIN;
-    assign OUT = tmp_OUT[31:0];
+    assign result       = in_data0 - in_data1 - ~in_carry;
+    assign out_data     = result[31:0];
 
-    assign COUT = ~ (~IN1[31] & OUT[31]);                       // borrow in the carry flag can only occur if the minuhend's
-                                                                // sign is 0 and result's sign is 1 
-    assign OVERFLOW = IN1[31] != IN2[31] && IN2[31] == OUT[31]; // overflow only occurs if the operands' signs are different
-                                                                // and the subtrahend's sign and the result's sign are the same
-    
+    assign out_carry    = ~ (~in_data0[31] & out_data[31]);  // borrow in the carry flag can only occur if the minuhend's
+                                                             // sign is 0 and result's sign is 1
+    assign out_overflow = in_data0[31] != in_data1[31] &&    // overflow only occurs if the operands' signs are different
+                          in_data1[31] == out_data[31];      // and the subtrahend's sign and the result's sign are the same
+                          
 endmodule

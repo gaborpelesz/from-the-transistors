aff4a9c68fc56cdaed7717973dc5125a61dfe9b7
Gabor <gaborpelesz@gmail.com>
2021-05-16 17:50:54 +0200

    split read and write in memory

--- cpu/src/memory.v
+++ cpu/src/memory.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
-/* Adjustable RAM, using Big-endian */
+/* Size adjustable RAM, with big-endian endianess */
 module memory #(parameter NUM_OF_BYTES = 800)(
-    input  wire        CLK,
+    input  wire        clk,
            wire [31:0] address,
            wire        read_en,
            wire        write_en,
@@ -10,23 +10,28 @@ module memory #(parameter NUM_OF_BYTES = 800)(
     output reg  [31:0] read_data
     );
     
-    reg [7:0] mem [0:NUM_OF_BYTES - 1]; // capable of storing 800 individual bytes 
+    reg [7:0] mem [0:NUM_OF_BYTES - 1]; // capable of storing 800 individual bytes by default
     
-    always @ (negedge CLK)
+    // write on posedge
+    always @ (posedge clk)
     begin
-        if (read_en && address < NUM_OF_BYTES-3)
-        begin
-            read_data <= { mem[address+3], mem[address+2], mem[address+1], mem[address] };
-        end else begin
-            read_data <= 32'bz;
-        end
-        
         if (write_en && address < NUM_OF_BYTES-3)
         begin
             mem[address]   <= write_data[7:0];
             mem[address+1] <= write_data[15:8];
             mem[address+2] <= write_data[23:16];
             mem[address+3] <= write_data[31:24];
+        end
+    end
+    
+    // read on negedge
+    always @ (negedge clk)
+    begin
+        if (read_en && address < NUM_OF_BYTES-3)
+        begin
+            read_data <= { mem[address+3], mem[address+2], mem[address+1], mem[address] };
+        end else begin
+            read_data <= 32'bz;
         end
     end
     

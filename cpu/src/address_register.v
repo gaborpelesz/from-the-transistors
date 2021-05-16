e743be3083ae13cfae9f791e1a387f09f3078e9e
Gabor <gaborpelesz@gmail.com>
2021-05-16 17:49:17 +0200

    remove clock input from modules

--- cpu/src/address_register.v
+++ cpu/src/address_register.v
@@ -1,27 +1,22 @@
 `timescale 1ns / 1ps
 
 module address_register(
-    input  wire        clk,
-           wire [31:0] in0, in1, in2,
+    input  wire [31:0] in0, in1, in2,
            wire [1:0]  in_select,
-           wire        perform_address_update,
-    output wire  [31:0] out_mem_address,
-           wire  [31:0] out_inc_address
+    output wire [31:0] out_mem_address,
+           wire [31:0] out_inc_address
     );
     
     reg [31:0] current_address = 32'b0;
     
-    always @ (posedge clk)
+    always @ (in_select, in0, in1, in2)
     begin
-        if (perform_address_update == 1'b1)
-        begin
-            case (in_select)
-                2'b00   : current_address <= in0;
-                2'b01   : current_address <= in1;
-                2'b10   : current_address <= in2;
-                default : current_address <= in2;
-            endcase
-        end 
+        case (in_select)
+            2'b00   : current_address = in0; // ALU bus
+            2'b01   : current_address = in1; // PC bus
+            2'b10   : current_address = in2; // incrementer bus
+            default : current_address = in2;
+        endcase 
     end
 
     assign out_mem_address = current_address;
--- cpu/src/reg_bank.v
+++ cpu/src/reg_bank.v
@@ -13,42 +13,68 @@
 
 */
 module reg_bank(
-    input  wire clk,
-           wire [3:0]  read_A_select,
-           wire [3:0]  read_B_select,
+    input  wire  [3:0] read_A_select,
+           wire  [3:0] read_B_select,
            wire        read_B_en,     // output to the B bus should be tri-state
-           wire [3:0]  write_select,
+           wire  [3:0] write_select,
            wire        write_en,
            wire [31:0] write_data,
            wire        write_pc_en,
            wire [31:0] write_pc_data,
+           wire  [3:0] write_cpsr_data,
+           wire        write_cpsr_en,
+           wire        reset,
     output wire [31:0] read_A_data,
            reg  [31:0] read_B_data,
-           wire [31:0] read_pc_data
+           wire [31:0] read_pc_data,
+           wire  [3:0] read_cpsr_data
     );
     
     localparam PC_SELECT = 4'd15;
     
-    reg [31:0] BANK [0:15];
+    reg [31:0] BANK [0:15]; // 16 x 32bit registers R0-R15, where R15 is the PC
+    reg  [3:0] cpsr;        // reduced Current Program Status Register, leaving only the N,Z,C,V bits
     
-    /* Write */
-    always @ (posedge clk)
+    /* Write registers */
+    always @ (*)
     begin
-        if (write_en)
-            BANK[write_select] <= write_data;
-
         // only write PC if we currently don't modify it from the ALU
         if (write_pc_en && !(write_select == PC_SELECT && write_en))
-            BANK[4'd15] <= write_pc_data;
-            
+            BANK[4'd15]        = write_pc_data;
+
+        else if (write_en)
+            BANK[write_select] = write_data;
+    end
+    
+    /* Write CPSR */    
+    always @ (*)
+    begin
+        if (write_cpsr_en)
+            cpsr = write_cpsr_data;
+    end
+    
+    /* Read register to B bus tri-state */
+    always @ (*)
+    begin
         if (read_B_en) // B bus tri-state
-            read_B_data <= BANK[read_B_select];
+            read_B_data = BANK[read_B_select];
         else
-            read_B_data <= 32'bz;
+            read_B_data = 32'bz;
     end
     
     /* Read lines */
-    assign read_A_data  = BANK[read_A_select];
-    assign read_pc_data = BANK[PC_SELECT];
+    assign read_A_data    = BANK[read_A_select];
+    assign read_pc_data   = BANK[PC_SELECT];
+    assign read_cpsr_data = cpsr;
+
+    /* Reset register bank */
+    integer i = 0;
+    always @ (posedge reset)
+    begin
+        for (i = 0; i < 16; i = i + 1)
+        begin
+            BANK[i] <= 32'b0;
+        end
+    end
 
 endmodule

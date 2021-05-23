04fdbf0283361c17efff35d3cdb07049d3b99151
Gabor <gaborpelesz@gmail.com>
2021-05-23 16:06:44 +0200

    new test program with endless loop and conditions

--- assembler/cpu_test.s
+++ assembler/cpu_test.s
@@ -1,7 +1,6 @@
-label: MOV R0, #5
-MOV R1, #15
-ADD R0, R0, R1  ;R0==20
-MOV R5, R1      ;R5==15
-ADD R0, R0, #1  ;++R0==21
-SUB R14, R0, R5 ;R14==6
-B label
+     MOV R0, #0     ; R0 = 0
+inc: ADD R0, R0, #1 ; ++R0
+     TEQ R0, #39    ; R0 == 39
+     BNE inc        ; if (R0 != 39) goto inc;
+nop: MOV R0, R0    ; else NOP => No OPeration
+     B nop          ; NOP forever

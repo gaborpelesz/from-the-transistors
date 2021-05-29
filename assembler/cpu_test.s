84b5c9bf443c627bec006bb59ba02363c3c7cd2d
Gabor <gaborpelesz@gmail.com>
2021-05-29 16:39:46 +0200

    fix rotate imm in example

--- assembler/cpu_test.s
+++ assembler/cpu_test.s
@@ -2,9 +2,11 @@
      
      ; constructing address in R13: 0x0038_0400
      MOV R13, #56, 16
-     ORR R13, R13, #4, 8
+     ORR R13, R13, #4, 24
 
-     STR R0, [R13, #+4] ; address: 0x0038_0404
+     STR R0, [R13], +R0, LSL #8 ; address: 0x0038_0404
+
+     LDR R1, [R13, -R0, LSL #8]! ; address: 0x0038_0404
 
 nop: MOV R0, R0     ; else NOP => No OPeration
      B nop          ; NOP forever

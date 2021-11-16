eabc59fcdf9439bb625d3e8db15b6a4683305540
Gabor Pelesz <gaborpelesz@gmail.com>
2021-11-16 11:46:33 +0200

    making scanner a cmake project

--- compiler_sandbox/scanner/src/scanner.c
+++ compiler_sandbox/scanner/src/scanner.c
@@ -11,10 +11,11 @@
  * 
  */
 
-#include "arrayi.h"
-#include "cstring.h"
+#include <cutils/arrayi.h>
+#include <cutils/string.h>
 
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // UTIL DEFINITIONS
 #define BOOL unsigned char
@@ -23,11 +24,12 @@
 
 // PRE-DEFINED GENERATED TABLES ---------
 
-// sizes are only imaginary for now
+// TODO sizes are only imaginary for now
+// TODO make these variables' scope local to this file
 BOOL accepting_states[10];        // indexed by state -> returns if the state is accepting (TRUE) or not (FALSE)
 short transition_table[10][10];   // indexed by a state (1st) and a char (2nd) -> returns the corresponding next state
 unsigned int classify_lexeme[10]; // indexed by accepting state -> returns index of token
-struct string* tokens[10];        // list of token names
+struct cutils_string* tokens[10];        // list of token names
 
 // --------------------------------------
 
@@ -41,61 +43,76 @@ struct string* tokens[10];        // list of token names
  *  - (MODIFIES)  analyzed_token_lexeme: the function creates a sequential list of lexemes as it discovers them in the text
  * 
  * Notes:
- *  The function appends the same amount of elements to both of the inputs that it modifies.
- *  The two arrays can be indexed with the same index to retreive the lexeme and its correspondent token.
- * 
+ *  The function appends the same amount of elements to both 'analyzed_token_indices' and 'analyzed_token_lexeme'.
+ *  (i.e., the two arrays can be indexed with the same index to retreive the lexeme and its correspondent token).
  */
-void scanner_skeleton_original(const struct string *const text,
-                               struct array * const analyzed_token_indices,
-                               struct array * const analyzed_token_lexemes) {
+void scanner_skeleton_original(const struct cutils_string *const text,
+                               struct cutils_arrayi * const analyzed_token_indices,
+                               struct cutils_string ** analyzed_token_lexemes) {
     // FA states:
     #define FA_STATE_INITIAL 0
     #define FA_STATE_ERROR -1
-    #define FA_STATE_BAD -2 // this gets pushed to the stack at first
+    #define FA_STATE_FIRST -2 // this gets pushed to the stack at first
 
-    int i = 0;
+    int text_i = 0;
 
-    struct string *lexeme = string_create();
-    struct arrayi *state_stack = array_create();
+    analyzed_token_lexemes = malloc(sizeof(struct cutils_string*) * 10);
+    unsigned int analyzed_token_lexemes_size = 10;
+    unsigned int analyzed_token_lexemes_n = 0;
+
+    struct cutils_string *lexeme = cutils_string_create();
+    struct cutils_arrayi *state_stack = cutils_arrayi_create();
     
     // Skeleton scanner FA table-driven simulation
     // original algorithm, can be optimized a lot
-    while (i < text->size) {
+    while (text_i < text->size) {
         short current_state = 0;
         
-        string_empty(lexeme);
+        cutils_string_empty(lexeme);
 
-        array_empty(state_stack);
-        array_push(state_stack, -1); // push "bad"
+        cutils_arrayi_empty(state_stack);
+        cutils_arrayi_push(state_stack, -1); // push "bad"
 
         // forward pass until either we do not reach
         //  - a state from where we can't go further, or
         //  - end of text
-        while (current_state != FA_STATE_ERROR || i < text->size) {
-            string_add_char(lexeme, string_at(text, i));
+        while (current_state != FA_STATE_ERROR || text_i < text->size) {
+            cutils_string_append_chr(lexeme, cutils_string_at(text, text_i));
 
             if (accepting_states[current_state]) {
-                array_empty(state_stack);
+                cutils_arrayi_empty(state_stack);
             }
 
-            array_push(state_stack, current_state);
+            cutils_arrayi_push(state_stack, current_state);
 
             // very simple dummy usage on transition table, might be changed later...
-            current_state = transition_table[current_state][string_at(text, i)];
+            current_state = transition_table[current_state][cutils_string_at(text, text_i)];
 
-            i = i+1; // next char
+            text_i += 1; // next char
         }
 
         // rollback loop -> trying to find longest prefix that is accepting
-        while (current_state != FA_STATE_BAD && !accepting_states[current_state]) {
-            current_state = array_pop(state_stack);
-            string_pop(lexeme); // we don't need to save output
-            i = i-1; // previous char
+        while (current_state != FA_STATE_FIRST && !accepting_states[current_state]) {
+            current_state = cutils_arrayi_pop(state_stack);
+            cutils_string_pop(lexeme); // we don't need to save output
+            text_i -= 1; // previous char
         }
 
         if (accepting_states[current_state]) {
-            array_push(analyzed_token_indices, classify_lexeme[current_state]);
-            array_push(analyzed_token_lexemes, lexeme);
+            cutils_arrayi_push(analyzed_token_indices, classify_lexeme[current_state]);
+
+            // This is just to append a lexeme at the end of the 'cutils_string' list (very disgusting)
+            // TODO create a dynamic array for this part...
+            {
+                analyzed_token_lexemes[analyzed_token_lexemes_n] = cutils_string_create_from(lexeme->_s);
+                analyzed_token_lexemes_n += 1;
+                // handle growing array's reallocation
+                if (analyzed_token_lexemes_n + 1 >= analyzed_token_lexemes_size) {
+                    analyzed_token_lexemes_size *= 2;
+                    analyzed_token_lexemes = realloc(analyzed_token_lexemes, sizeof(*analyzed_token_lexemes) * analyzed_token_lexemes_size);
+                }
+            }
+            // end of TODO
         }
     }
 }

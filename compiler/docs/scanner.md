2ac0e4acecda9badb3c381c0d7ea6725c4a34fda
Gabor Pelesz <gaborpelesz@gmail.com>
2021-12-01 02:02:09 +0200

    documenting and creating regex lang

--- compiler/docs/scanner.md
+++ compiler/docs/scanner.md
@@ -1,3 +1,39 @@
 # The scanner of the compiler
 
-*TODO: List attributes of the scanner, e.g., "hand-crafted or generated", "backtrack-free or not", "parallel or not"...*
+## .lang files and regex
+This is an example of the language syntax description files for the compiler's scanner. 
+The syntax was mostly inspired by the lex style syntax description files, however, it is very much simplified and far not as powerful.
+
+```lang
+# This is a comment
+
+# This is a TOKEN definition with a regex
+DIGIT : [0-9]
+
+# The TOKEN definitions are evaluated in order
+# Higher definitions gets more priority
+REGISTER : r[0-9]+
+IDENTIFIER : [a-zA-Z][a-zA-Z_0-9]*
+```
+
+Definition of the regular expression language:
+- concatenation: ab
+- alteration: a|b
+- closure (0 or any no.): a*
+- parentheses: (a|b)(a|c)*
+- positive closure: a+ == aa*
+- empty string special character: \e
+- zero or one occurance: a? == (\e | a)
+- wildcard (matches any char in alphabet): a.b == a(a|b|c|...)b
+- shorthands:
+  - `[0-9] == (0|1|2|3|4|5|6|7|8|9)`
+  - `[a-z] == (a|b|...|z)`
+  - `[A-Z] == (A|B|...|Z)`
+  - `[a-Z] == [a-z]|[A-Z] == (a|b|...|z|A|B|...|Z)`
+  - arbitrary range: `[c-L][4-8]`
+- match any whitespace character (\t, \r, \n, spaces): a\sb -> \s
+- special character arithmetic: (a|b)* == a*b*
+- precedence: parentheses ( (a|b)* ), closure (a*), concatenation (ab), alteration (a|b)
+- special characters: ", \\, |, (, ), *, +, ?, ., [, ], \s, \t, \r, \n
+- special character handling:
+  - place things inside quotes: "something with | (special characters)" will match exactly what's inside
--- compiler/scanner/src/generator.c
+++ compiler/scanner/src/generator.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <cutils/string.h>
+#include <cutils/arrayi.h>
 
 // What do we need to generate?
 //  - FA states: #define FA_STATE_INITIAL 0; etc...
@@ -10,8 +12,55 @@
 //  - tokens list with names
 //    - this is the easiest I guess
 
+static const struct cutils_string* regex_token_map[6]; 
+
+typedef int REGEX_SCANNER_STATUS;
+REGEX_SCANNER_STATUS regex_scanner(const struct cutils_string * const rgx, struct cutils_arrayi *tokens) {
+    printf("trying regex: %s\n", rgx->_s);
+
+    int retval = 0;
+
+    int parenthesis_cnt = 0;
+    int bracket_cnt = 0;
+    int quote_cnt = 0; // when update do: quote_cnt = !quote_cnt
+
+    // scanning...
+    // ... add characters to a lexeme until we find an special character. The token for the lexeme: concatenated_chars
+    // ... every string that's inside two quotes should be a concatenated_chars immediately
+    // ... if the string inside two quotes contains a quote character it should be escaped with a backslash "\"this\"" matches -> "this"
+
+    if (parenthesis_cnt != 0) {
+        if (parenthesis_cnt < 0) {
+            printf("Unmatched parenthesis inside regex: too much closing ')'");
+        } else { // (parenthesis_cnt < 0)
+            printf("Unmatched parenthesis inside regex: too much opening '('");
+        }
+        retval = 1;
+    }
+    if (bracket_cnt != 0) {
+        if (bracket_cnt < 0) {
+            printf("Unmatched brackets inside regex: too much closing ')'");
+        } else { // (parenthesis_cnt < 0)
+            printf("Unmatched brackets inside regex: too much opening '('");
+        }
+        retval = 1;
+    }
+    if (quote_cnt != 0) {
+        printf("Unmatched quotes inside regex: too much closing '\"'");
+        retval = 1;
+    }
+    return retval;
+}
+
 // for testing
 int main(void) {
-    printf("hello world\n");
+    regex_token_map[0] = cutils_string_create_from("(");
+
+    struct cutils_string *rgx = cutils_string_create_from("(ab|b)*");
+    struct cutils_arrayi *tokens = cutils_arrayi_create();
+
+    regex_scanner(rgx, tokens);
+
+    cutils_string_destroy(rgx);
     return 0;
 }

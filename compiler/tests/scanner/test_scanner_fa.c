4f997e1b78d38645b24464112110b37e248af495
Gabor Pelesz <gaborpelesz@gmail.com>
2022-03-01 09:24:26 +0200

    prepare scanner FA tests

--- compiler/tests/scanner/test_scanner_fa.c
+++ compiler/tests/scanner/test_scanner_fa.c
@@ -3,6 +3,26 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
+#include <cutils/arrayi.h>
+#include <scanner_utils/fa.h>
+
+static void test_scanner_fa_create(void **state) {
+    
+}
+
+static void test_1(void **state) {
+
+}
+
+static void test_2(void **state) {
+
+}
+
+
+static void test_3(void **state) {
+
+}
+
 static void test_fail(void **state) {
     assert_true(0);
 }
